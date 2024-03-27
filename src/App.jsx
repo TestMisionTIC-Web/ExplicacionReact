fa86ee9e5755bc8fe61e8a90f1db2adf64bbc9b6
brincon <brendarin10@gmail.com>
2024-03-27 15:53:17 -0500

    crear card de razas de perros

--- src/App.jsx
+++ src/App.jsx
@@ -29,36 +29,35 @@ function App() {
         <section>
             <h1 className="title">RAZAS DE PERROS</h1>
             <ul className="breedCardContainer">
-                <li className="breedCard">
+                <CardRazasPerros nombreRaza = "Border Collie" imagen = {boderCollie}/>
+                <CardRazasPerros nombreRaza = "Rhodesian" imagen = {rhodesian}/>                  
+            </ul>
+        </section>
+        <section></section>
+    <footer>
+        
+    </footer>
+    </div>
+  );
+}
+
+function CardRazasPerros({nombreRaza, imagen}) {
+    return(
+        <li className="breedCard">
                     <div className="contenedorImagen">
-                        <img src={boderCollie} alt="Border Collie"/>
+                        <img src={imagen} alt={nombreRaza}/>
                     </div>
-                    <span className="breedTitle">Border Collie</span>
+                    <span className="breedTitle">{nombreRaza}</span>
                     <div className="endBreedCard">
                         <i className="fa-solid fa-heart">150</i>
                         <i className="fa-solid fa-comment">1.5k</i>
                         <i className="fa-solid fa-eye">120</i>
                     </div>
-                </li>                    
-                
-                <li className="breedCard">
-                    <div className="contenedorImagen">
-                        <img src={rhodesian} alt="Rodesian"/>
-                    </div>
-                    <span className="breedTitle">Rhodesian</span>
-                    <div className="endBreedCard">
-                        <i className="fa-solid fa-heart">450</i> 
-                        <i className="fa-solid fa-comment">1.1k</i>
-                        <i className="fa-solid fa-eye">850</i>
-                    </div>
-                                     
-                </li>
-            </ul>
-        </section>
-        <section></section>
-    <footer></footer>
-    </div>
-  );
+                </li>   
+
+    )
+
 }
+    
 
 export default App;
